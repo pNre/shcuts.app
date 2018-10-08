359134390aa18edb3c54577225b47fa43aadd9d1
Pierluigi <mail@pierluigi.me>
2018-10-08 10:16:02 +0200

    Add GA

---
+++ src/@types/vue-analytics/index.d.ts
@@ -0,0 +1,4 @@
+declare module "vue-analytics" {
+    import Vue, { PluginFunction } from 'vue';
+    export const install: PluginFunction<{}>
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import App from '@/App.vue';
 import Router from '@/Router';
 import Store from '@/Store';
 
 Vue.config.productionTip = false;
+Vue.use(VueAnalytics, {
+    id: 'UA-62862441-2',
+});
 
 new Vue({
     router: Router,
